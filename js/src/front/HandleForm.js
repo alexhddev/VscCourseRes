3071f73394a31b0bb890e12170ab3a26ba568705
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-11-15 09:15:01 +0200

    Stuff

--- js/src/front/HandleForm.js
+++ js/src/front/HandleForm.js
@@ -1,22 +1,22 @@
-function handleForm(){
-    const getByIdForm = document.getElementById('cool-form');
-    const documentForm = document.forms[0];
+function handleForm() {
+    const getByIdForm = document.getElementById('cool-form')
+    const documentForm = document.forms[0]
 
-    documentForm.addEventListener('submit', (event)=>{
-        event.preventDefault();
-        const name = documentForm.elements['name']?.value;
+    documentForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const name = documentForm.elements['name']?.value
         checkName({})
-        const email = documentForm.elements['email']?.value;
+        const email = documentForm.elements['email']?.value
         console.log('stuff')
     })
 }
 
-handleForm();
+handleForm()
 
 /**
  * @param {string} name the name
  */
-function checkName(name){
+function checkName(name) {
     if (typeof name === 'string' && name.length > 3) {
         console.log('Valid name')
     } else {
