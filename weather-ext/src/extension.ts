1bdc9f624df76ab74ef32b89ec2a0bf457c17000
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-12-04 13:54:52 +0200

    Status bar item

--- weather-ext/src/extension.ts
+++ weather-ext/src/extension.ts
@@ -2,19 +2,28 @@
 import * as vscode from 'vscode';
 import { DataService } from './services/DataService';
 
+let weatherStatusBarItem: vscode.StatusBarItem;
+
 
 export function activate(context: vscode.ExtensionContext) {
 
 	const dataService = new DataService();
 
-	let disposable = vscode.commands.registerCommand('weather-ext.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('weather-ext.getWeather', () => {
 
 		vscode.window.showInputBox({
 			value: 'Please enter city'
 		}).then(async city =>{
 			if (city) {
 				const result = await dataService.getWeather(city);
-				console.log(result)
+				if (!weatherStatusBarItem) {
+					weatherStatusBarItem = vscode.window.createStatusBarItem(
+						vscode.StatusBarAlignment.Right,
+						100
+					)
+				}
+				weatherStatusBarItem.text = `${result.city} ${result.temperature} ${result.description}`
+				weatherStatusBarItem.show();
 			}
 		})
 	});
