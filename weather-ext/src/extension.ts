940f1ed716bcc9fbb7ec4645a61b3eefcbbe1f54
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-12-04 13:43:30 +0200

    DataService

--- weather-ext/src/extension.ts
+++ weather-ext/src/extension.ts
@@ -1,12 +1,22 @@
 
 import * as vscode from 'vscode';
+import { DataService } from './services/DataService';
 
 
 export function activate(context: vscode.ExtensionContext) {
 
+	const dataService = new DataService();
+
 	let disposable = vscode.commands.registerCommand('weather-ext.helloWorld', () => {
 
-		vscode.window.showInformationMessage('Hello World from weather-ext!');
+		vscode.window.showInputBox({
+			value: 'Please enter city'
+		}).then(async city =>{
+			if (city) {
+				const result = await dataService.getWeather(city);
+				console.log(result)
+			}
+		})
 	});
 
 	context.subscriptions.push(disposable);
---
+++ weather-ext/src/services/DataService.ts
@@ -0,0 +1,28 @@
+const apiKey = '323a805c82f4e444c4d6eb02dd3cc09f';
+
+export interface CityWeather  {
+    city: string;
+    temperature: number;
+    description: string,
+    lat: number;
+    lon: number;
+}
+
+export class DataService {
+
+    public async getWeather(city: string): Promise<CityWeather>{
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
+        const response = await fetch(url, {
+            method: 'GET'
+        })
+        const parsedResponse = await response.json() as any;
+        return {
+            description: parsedResponse.weather[0].main,
+            temperature: parsedResponse.main.temp,
+            city: parsedResponse.name,
+            lon: parsedResponse.coord.lon,
+            lat: parsedResponse.coord.lat
+        }
+
+    }
+}
